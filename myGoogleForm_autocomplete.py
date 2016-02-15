7874c257c612e22bf116ff962755331f3db7eb8d
kiriashoo <kiriashoo@gmail.com>
2016-02-15 15:54:20 +0200

    complete a Google Form with Python Requests
    first example - complete a Google Form with Python Requests

---
+++ myGoogleForm_autocomplete.py
@@ -0,0 +1,19 @@
+''' My GOOGLE FORM Test Auto-complete '''
+
+import requests
+
+payload = {
+    'entry.1371035425': 'ELVIS PRESLEY',
+    'entry.1757680857': 'https://erepublik.com/en'
+}
+
+import sys
+
+with requests.Session() as c:
+    r = c.post('https://docs.google.com/forms/d/1PkHsFwu8dJ_qYpun8DskfgCub9piEF5LvIYxkAUIXws/formResponse', data=payload)
+    print 'Your response has been recorded.' in r.content
+    print r.url
+    print r.status_code
+    print r.reason
+    print r.elapsed  
+    print r.content
